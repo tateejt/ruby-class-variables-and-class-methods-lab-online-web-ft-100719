b384de62e995eab4f44f3f9e75a2797d071e3187
Emery Tate <tateejt@gmail.com>
2019-10-18 02:41:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+  end
+  
+  def genres
+    
+end
